65510e67f38dd373fa9829226fc460a1a67809ea
Rahul Kumar <rahul@gmail.com>
2024-08-23 08:25:53 +0545

    final Nav

--- app/src/main/java/com/example/navigation_project/MainActivity.java
+++ app/src/main/java/com/example/navigation_project/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.navigation_project;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +19,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+
+        DrawerLayout drawerLayout=findViewById(R.id.drawer);
+        ImageView imageView=findViewById(R.id.nav_img1);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
 
     }
 }
