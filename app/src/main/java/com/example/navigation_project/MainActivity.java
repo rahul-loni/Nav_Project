eddad15ad10ad661ba8ac666e16d8c4ba7bfc979
Rahul Kumar <rahul@gmail.com>
2024-08-23 08:32:42 +0545

    menu item click

--- app/src/main/java/com/example/navigation_project/MainActivity.java
+++ app/src/main/java/com/example/navigation_project/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.navigation_project;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +33,24 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+
+        // nav items click
+        NavigationView navigationView=findViewById(R.id.nav);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                int id=menuItem.getItemId();
+                if(id==R.id.item1){
+                    Toast.makeText(MainActivity.this, "Click Item 1", Toast.LENGTH_SHORT).show();
+                }
+                if (id==R.id.item2){
+                    Toast.makeText(MainActivity.this, "Click Item 2", Toast.LENGTH_SHORT).show();
+                }
+                return false;
             }
         });
 
